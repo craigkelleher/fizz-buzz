7496a26f946403f64339266dbb3badc8e2d2a18a
craigkelleher <kelleher.craig@gmail.com>
2022-09-08 12:04:22 -0700

    Updated for enum class

--- src/main/java/com/tlglearning/FizzBuzz.java
+++ src/main/java/com/tlglearning/FizzBuzz.java
@@ -1,23 +1,41 @@
 package com.tlglearning;
 
-public class FizzBuzz {
-  public static final String FIZZ_RESULT = "fizz";
-  public static final String BUZZ_RESULT = "buzz";
+import java.util.EnumSet;
+import java.util.Set;
 
-  public static String valueOf(int num){
-    String result = "";
-    if (num % 3 == 0){
-      result += FIZZ_RESULT;
+// Changing class to enum.
+public enum FizzBuzz {
+/*  public static final String FIZZ_RESULT = "fizz";
+    public static final String BUZZ_RESULT = "buzz";
+    enumerated values: commas between them, can be on separate lines
+    FIZZ, BUZZ;
+    Fizz()  if we have arguments, we but them on separate lines
+    Buzz;*/
+
+/*  adjusting to have it return a set of 1, both, or neither of fizz and buzz
+    neither: return empty set, UI will tell the user the number.
+    public static String valueOf(int num){*/
+  public static Set<FizzBuzz> valueOf(int num){
+/*    Enum is a particular side of set (box)
+      .class is an object reference here. its how we refer to objects*/
+    Set<FizzBuzz> result = EnumSet.noneOf(FizzBuzz.class);
+    //String result = "";
+    if (num % 3 == 0) {
+      //result += FIZZ_RESULT;
+      result.add(FIZZ);
     }
-    if (num % 5 == 0){
-      result += BUZZ_RESULT;
+    if (num % 5 == 0) {
+      //result += BUZZ_RESULT;
+      result.add(BUZZ);
     }
-    if (result.isEmpty()){
+/*
+    if (result.isEmpty()) {
       result = String.valueOf(num);
       // same as result += num
       // same as result = result + num
       // need to convert num to string, above do that.
     }
+*/
     return result;
   }
 }
--- src/main/java/com/tlglearning/Main.java
+++ src/main/java/com/tlglearning/Main.java
@@ -1,15 +1,20 @@
 package com.tlglearning;
 
+import java.util.Set;
+
 public class Main {
 
   public static void main(String[] args) {
     int i = 1;
     do {
-      String value =FizzBuzz.valueOf(i);
-      System.out.println(value);
-      if (value.equals("fizzbuzz")){
+      //String value =FizzBuzz.valueOf(i);
+      Set<FizzBuzz> value =FizzBuzz.valueOf(i);
+      //%d integer expressed in base 10 format
+      System.out.printf("%d: %s%n",i, value);
+/*      if (value.equals("fizzbuzz")){
         break;
-      }
+      }*/
+
       i++;
     } while (i <= 100);
   }
