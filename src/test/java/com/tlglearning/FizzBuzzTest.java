29a163a77d1efe725dd02b9b8cfb81a5a6eca88d
craigkelleher <kelleher.craig@gmail.com>
2022-09-20 12:44:18 -0700

    Updated for assert tests

--- src/test/java/com/tlglearning/FizzBuzzTest.java
+++ src/test/java/com/tlglearning/FizzBuzzTest.java
@@ -2,11 +2,35 @@ package com.tlglearning;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Collections;
+import java.util.Set;
 import org.junit.jupiter.api.Test;
 
 class FizzBuzzTest {
 
   @Test
-  void valueOf() {
+  void valueOf_fizz() {
+    //assert stmt that if boolean condition is false, this stmt will throw assertion error
+    //assert Set.of(FizzBuzz.FIZZ).equals(FizzBuzz.valueOf(6));3
+    //compare first value, fizz, against a method invocation or we aren't testing a method
+    assertEquals(Set.of(FizzBuzz.FIZZ), FizzBuzz.valueOf(6));
+    assertEquals(Set.of(FizzBuzz.FIZZ), FizzBuzz.valueOf(9));
+  }
+
+  @Test
+  void valueOf_buzz() {
+    //equal method is for comparing quality of state.
+    assertEquals(Set.of(FizzBuzz.BUZZ), FizzBuzz.valueOf(10));
+  }
+
+  @Test
+  void valueOf_fizzBuzz(){
+    assertEquals(Set.of(FizzBuzz.FIZZ, FizzBuzz.BUZZ), FizzBuzz.valueOf(15));
+  }
+
+  @Test
+  void valueOf_none(){
+    //assertEquals(Set.of());
+    assertEquals(Set.of(), FizzBuzz.valueOf(11));
   }
 }
